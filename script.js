c5eacda9ddf737de85967e36a75c756688786b50
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-06 15:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+let div = document.getElementById('app');
+let button = document.getElementById('swap');
 
+button.addEventListener('click', function() {
+  if (div.style.background === 'black') {
+    div.style.background = 'white';
+  } else {
+    div.style.background = 'black';
+  }
+});
