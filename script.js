846dbc828c52c8bb55aba08320825c96bcef3d3f
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-06 16:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your code here
+
 let div = document.getElementById('app');
 let button = document.getElementById('swap');
 
@@ -6,13 +7,14 @@ button.addEventListener('click', function() {
   if (div.classList.contains('day')) {
     div.classList.remove('day');
     div.classList.add('night');
-    button.classList.remove('button_night');
-    button.classList.add('button_day');
+    button.classList.remove('button_day');
+    button.classList.add('button_night');
   } else {
     div.classList.remove('night');
     div.classList.add('day');
-    button.classList.remove('button_day');
-    button.classList.add('button_night');
+    button.classList.remove('button_night');
+    button.classList.add('button_day');
   }
 });
+
 
