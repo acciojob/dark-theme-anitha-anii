002596ac0651724297b51f859309eeb9b73756f1
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-06 16:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ let div = document.getElementById('app');
 let button = document.getElementById('swap');
 
 button.addEventListener('click', function() {
-  if (div.style.background === 'black') {
-    div.style.background = 'white';
+  if (div.classList.contains('day')) {
+    div.classList.remove('day');
+    div.classList.add('night');
+    button.classList.remove('button_night');
+    button.classList.add('button_day');
   } else {
-    div.style.background = 'black';
+    div.classList.remove('night');
+    div.classList.add('day');
+    button.classList.remove('button_day');
+    button.classList.add('button_night');
   }
 });
+
